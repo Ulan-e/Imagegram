5a54be77520aa2ddb77ab0522810f182c4ac7f08
Us <johndoe@example.com>
2021-04-16 13:12:33 +0300

    Code refactoring

--- app/src/main/java/ulanapp/imagegram/data/api/ApiService.kt
+++ app/src/main/java/ulanapp/imagegram/data/api/ApiService.kt
@@ -5,10 +5,6 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 import ulanapp.imagegram.data.model.PhotosResponse
 
-/**
- * API KEY = 563492ad6f917000010000016f8f084d5a7f487a8f9b64ee49229734
- *
- */
 interface ApiService {
 
     @GET("api/")
